5b1c412558602ac61016bc88625b918a81284c76
강병국 <kang9305@naver.com>
2020-08-30 23:42:31 +0700

    20.08.30 알고리즘

--- SSAFY/D3/10570. 제곱 팰린드롬 수.py
+++ SSAFY/D3/10570. 제곱 팰린드롬 수.py
@@ -1,4 +1,9 @@
-for i in range(1, int(input()+1)):
+for t in range(1, int(input())+1):
     A, B = map(int, input().split())
+    count = 0
     for num in range(A, B+1):
-        if A <= num**2 <=
+        if num**(1/2) == int(num**(1/2)):
+            if str(num) == str(num)[::-1]:
+                if str(int(num**(1/2))) == str(int(num**(1/2)))[::-1]:
+                    count += 1
+    print('#{} {}'.format(t, count))
