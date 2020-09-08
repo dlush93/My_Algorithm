cb207a57a4ecae1b0b66b05c4e6d18a71388a21b
dlush93 <qudrnr9305@gmail.com>
2020-09-08 20:37:35 +0500

    20.09.08 알고리즘

--- BAEKJOON/Bronze/B1/1236. 성 지키기.py
+++ BAEKJOON/Bronze/B1/1236. 성 지키기.py
@@ -1,1 +1,18 @@
 N, M = map(int, input().split())
+castle = [list(input()) for i in range(N)]
+security = []
+for i in range(N):
+    for j in range(M):
+        if castle[i][j] == 'X':
+            security.append([i,j])
+if N <= M :
+    security_list = [1]*(N+1)
+    for i in security:
+        security_list[i[0]] = 0
+    print(sum(security_list)-1)
+
+else:
+    security_list = [1]*(M+1)
+    for i in security:
+        security_list[i[1]] = 0
+    print(sum(security_list)-1)
--- BAEKJOON/Bronze/B1/1268. 임시 반장 정하기.py
+++ BAEKJOON/Bronze/B1/1268. 임시 반장 정하기.py
@@ -1,3 +1,22 @@
 student = int(input())
-array = [[0]*student for i in range(student)]
 table = [list(map(int, input().split())) for i in range(student)]
+array = [[] for i in range(student)]
+for i in range(student):
+    for j in range(5):
+        class_num = table[i][j]
+        for k in range(student):
+            if i == k:
+                continue
+            else:
+                if table[k][j] == class_num:
+                    if k not in array[j]:
+                        array[j].append(k)
+                        print(i, j, k, array)
+class_mate = 0
+temp = 0
+
+for index, friends in enumerate(array, 1):
+    if len(friends) > class_mate:
+        temp = index
+print(temp)
+print(array)
