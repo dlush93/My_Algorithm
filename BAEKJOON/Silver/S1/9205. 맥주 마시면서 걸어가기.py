229a432c3cc2a9047fc79e1dad75f89fc1d5182a
dlush93 <qudrnr9305@gmail.com>
2021-07-28 10:36:34 +0900

    07-28 Algorithm(am)

--- BAEKJOON/Silver/S1/9205. 맥주 마시면서 걸어가기.py
+++ BAEKJOON/Silver/S1/9205. 맥주 마시면서 걸어가기.py
@@ -1,26 +1,20 @@
-def distance_chk(now, end):
-    nx, ny = now
-    ex, ey = end
-    dx, dy = abs(nx-ex), abs(ny-ey)
-    if (dx + dy)/50 > 20:
-        return False
-    else:
-        if abs(rock_x-nx) + abs(rock_y-ny) < abs(rock_x-ex) + abs(rock_y-ey):
-            return False
-        else:
-            return True
+def most_nearest_conven():
+    near_distance = 1000
+    near_con = []
+    for con in conven:
+        if abs(now[0] - con[0]) + abs(now[1] - con[1]) < near_distance:
+            near_con.append([con[0], con[1]])
 
 
 for t in range(int(input())):
     n = int(input())
-    beer = 20
-    arr = [[0]*65536 for i in range(65536)]
-    store = []
-    home_x, home_y = map(int, input().split())
-    arr[home_x+32768][home_y+32768] = 1
-    for i in range(n):
-        x, y = map(int, input())
-        store.append([x, y])
-        arr[x+32768][y+32768] = 2
-    rock_x, rock_y = map(int, input().split())
-    arr[rock_x+32768][rock_y+32768] = 3
+    home = list(map(int, input().split()))
+    now = [i for i in home]
+    conven = set(tuple(map(int, input().split())) for i in range(n))
+    rock = list(map(int, input().split()))
+    distance = abs(home[0] - rock[0]) + abs(home[1] - rock[1])
+    if distance <= 1000:
+        print('happy')
+    else:
+        pass
+
